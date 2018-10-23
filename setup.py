65c9683165506daadc85e77c6f8a9b7ba3865e33
Timofey Kukushkin <t.kukushkin@cian.ru>
2018-10-23 22:08:18 +0300

    Add README to pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
+import os
+
 from setuptools import find_packages, setup
 
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='aioamqp-consumer-best',
     version='1.1.0',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=[
