63fb6bc7ab5271f153bc464cf35fb8d96c9e808c
Timofey Kukushkin <t.kukushkin@cian.ru>
2019-04-24 08:47:54 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,13 @@ with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='aioamqp-consumer-best',
     version='1.1.2',
+    url='https://github.com/tkukushkin/aioamqp-consumer-best',
+    author='Timofey Kukushkin',
+    author_email='tima@kukushkin.me',
+    description='Consumer utility for AMQP',
     long_description=long_description,
     long_description_content_type='text/markdown',
+    license='MIT',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=[
@@ -21,7 +26,7 @@ setup(
     ],
     extras_require={
         'test': [
-            'mypy==0.600',
+            'mypy',
             'pycodestyle',
             'pylint',
             'pytest',
@@ -29,5 +34,22 @@ setup(
             'pytest-cov',
             'pytest-mock',
         ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: AsyncIO'
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Typing :: Typed',
+    ],
+    project_urls={
+        'Source': 'https://github.com/tkukushkin/aioamqp-consumer-best',
     },
 )
