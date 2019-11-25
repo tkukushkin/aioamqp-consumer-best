3eebd5ffa5cdcdff7d11ae3d402849fd1393bbaf
Timofey Kukushkin <t.kukushkin@cian.ru>
2019-11-25 13:33:29 +0300

    Fix pylint

--- src/aioamqp_consumer_best/_helpers.py
+++ src/aioamqp_consumer_best/_helpers.py
@@ -7,6 +7,6 @@ from typing import AsyncIterator, TypeVar
 T = TypeVar('T')
 
 
-async def queue_to_iterator(queue: asyncio.Queue[T]) -> AsyncIterator[T]:
+async def queue_to_iterator(queue: asyncio.Queue[T]) -> AsyncIterator[T]:  # pylint: disable=unsubscriptable-object
     while True:
         yield await queue.get()
